e8b695152c6baab0417af3b7ad5ac27c4bc8ced6
jjacquot <jean-paul.jacquot@withings.com>
2023-10-17 15:46:43 +0200

    add models

---
+++ app/src/main/java/com/example/myapplication/data/models/Location.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication.data.models
+
+data class Location(
+    val name: String,
+    val long: Long,
+    val lat: Long,
+)
--- app/src/main/java/com/example/myapplication/data/models/User.kt
+++ app/src/main/java/com/example/myapplication/data/models/User.kt
@@ -3,5 +3,5 @@ package com.example.myapplication.data.models
 data class User(
     val id: String,
     val lastName: String,
-    val firsName: String,
+    val firstName: String,
 )
