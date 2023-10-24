430f73d5bc2731a333237bdd9352e59ffadbc64f
jjacquot <jean-paul.jacquot@withings.com>
2023-10-24 15:23:02 +0200

    pause

--- app/src/main/java/com/example/myapplication/composables/MainActivityScreen.kt
+++ app/src/main/java/com/example/myapplication/composables/MainActivityScreen.kt
@@ -1,14 +1,22 @@
 package com.example.myapplication.composables
 
-import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Share
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.example.myapplication.MainViewModel
 import com.example.myapplication.data.models.SharedMetadata
@@ -27,7 +35,7 @@ fun MainScreen(mainViewModel: MainViewModel) {
         )
 
         Column {
-            //  BatteryLevel(useCase.getBatteryLevel())
+            TopBar()
             sharedMetadata?.let { MetadataViewer(it) }
         }
     }
@@ -39,4 +47,35 @@ fun MetadataViewer(metadata: SharedMetadata, modifier: Modifier = Modifier) {
         text = metadata.userId,
         modifier = modifier
     )
+}
+
+@Composable
+fun TopBar() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.End
+    ) {
+
+        followedUserList()
+        IconButton(onClick = { }) {
+            Icon(
+                Icons.Rounded.Share,
+                contentDescription = "Partagez"
+            )
+        }
+    }
+}
+
+val listItems = listOf("Klara", "Mark", "Sonia", "Paul")
+
+@Composable
+fun followedUserList() {
+    DropdownMenu(expanded = true, onDismissRequest = { false }) {
+        listItems.forEachIndexed { itemIndex, itemValue ->
+            DropdownMenuItem(
+                onClick = {},
+                text = { Text(text = itemValue) }
+            )
+        }
+    }
 }
